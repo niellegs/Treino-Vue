70a8be7ef5609004ffa9a39d91cfa006159fec3e
Danielle Gomes <108994881+ughhlynx@users.noreply.github.com>
2023-05-01 19:56:24 -0300

    tailwind css

---
+++ exercises/ex001/my-project/src/components/Calculator.vue
@@ -0,0 +1,29 @@
+<template>
+
+<div class="h-screen bg-green-400 flex justify-center py-16">
+
+    <div class="w-1/3">
+
+        <div class="p-4 text-right shadow-2xl bg-white rounded-t-lg">
+            <p class="font-semibold text-gray-700 h-6">4535</p>
+            <p class="font-bold text-3xl text-gray-900 my-2">0</p>
+        </div>
+
+        <div class="">
+
+            <button class="h-16 text-xl">AC</button>
+
+        </div>
+
+    </div>
+
+</div>
+
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
